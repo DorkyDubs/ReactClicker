4c3e9aeef295028b4cae1581b82a93e686dddcf0
Dorkydubs <rsgacre@gmail.com>
2024-06-28 10:37:47 +0100

    Walk-in skelly for react cookieclicker

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+//! project is about organising and planning elements into folders and components
+
+import { useEffect, useState } from "react";
+
+import "./App.css";
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  // need 2 global variables: one to store  counts an one to store  per second
+
+  const [cookies, setCookies] = useState(0);
+  const [cookiesPerSecond, setCookiesPerSecond] = useState(0);
+  const [secretTimer, setSecretTimer] = useState(0);
+  // upgardes should increase perSecond and decrease count
+
+  //!Stretch: fetch API
+
+  const upgrades = [{}, {}];
+  // need timer to track count value, monitoring per second
+  // count goes up by perSecond value
+  useEffect(() => {
+    const theBiscuitIntervalTimer = setInterval(() =>
+      //need to tell interval what action to perform every second
+      {
+        setCookies((currentCookies) => currentCookies + cookiesPerSecond);
+        setSecretTimer((currentTime) => currentTime + 1);
+      }, 1000);
+
+    return () => {
+      clearInterval(theBiscuitIntervalTimer);
+    };
+  }, [cookiesPerSecond]); // add persecond in the dpeendency array so when its value changes the useEffect triggers again
+
+  // need a function so user can buy items from shop
+
+  function attemptPurchase(cost, increase) {
+    //increase value of perSecond
+    // decrease count
+  }
+
+  return (
+    <>
+      {/* need to display :
+  Value of count
+  value of perSecond
+  element that user can click to increase count
+  list of (mapped) shop items the user can buy [//!stretch add visual feedback to show if user can buy it (conditional rendering)//
+  ].
+  
+  */}
+    </>
+  );
+}
+
+export default App;
---
+++ src/libs/UpgradeStore.js
@@ -0,0 +1,11 @@
+export let biscuitUpgrades = [
+  {
+    id: 1,
+    name: "Weekend Biscuit Intern",
+    cost: 100,
+    toIncrease: 100,
+    // increaseFunction: function increase(this.toIncrease){
+
+    // }
+  },
+];
